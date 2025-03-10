6340b7299c5246ecd73f1a9c6f7a8beb0244bc84
seahpark247 <famousjedo@gmail.com>
2025-03-09 20:35:47 -0500

    Initial Commit

---
+++ TicTacToeTests/TicTacToeTests.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeTests.swift
+//  TicTacToeTests
+//
+//  Created by Seah Park on 3/9/25.
+//
+
+import Testing
+@testable import TicTacToe
+
+struct TicTacToeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
